38c3f5fc78573daeb547a1c2cb4ccf65d968a76a
code-with-santanu <santanupal003@gmail.com>
2022-01-08 16:35:25 +0530

    updated

--- simple calculator.c
+++ simple calculator.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 #include<math.h>
 
-int add(int);
-int sub(int);
-int mult(int);
-int div(int);
-int power(int);
+int add(int,int);
+int sub(int,int);
+int mult(int,int);
+int div(int,int);
+int power(int,int);
                        
 int add(int a,int b)
 {                   
@@ -45,70 +45,41 @@ int power(int a,int b)
 
 int main()
 {
-	int a,b,choice;
-	printf("\nEnter the input nos: ");
-	scanf("%d\t%d",&a,&b);
-	printf("press any one operation from the following: '+', '-', '*', '/', '^'");
+	int a,b,result;
+	char d;
+	printf("\nEnter the input no: ");
+	scanf("%d",&a);
+	printf("press any one operation from the following: '+', '-', '*', '/', '^': ");
 	scanf("%c",&d);
-	
-	//To sum
-	if(d == '+')
+	result=a;
+	while(d!= '=')
 	{
-		result=add(a,b);
-		printf("\nDo you want to add more no?\nPress 1 for YES\nPress 2 for NO");
-		scanf("%d",&choice);
-		
-		if(choice==1)
+		printf("\nEnter the next no: ");
+		scanf("%d",&b);
+		switch(d)
 		{
-			printf("\nEnter new no: ");
-			scanf("%d",&a);
+			case '+': 
+			{
+				result=add(result,b);
 			
+			}
+			case '-':
+			{
+				result=sub(result,b);
+				
+			}
+			
+			case '*':
+			{
+				result=mult(result,b);
+			
+			}
 		}
 		
-		else if(choice==0)
-		{
-			printf("\nTotal= %d",result);
-		}
-		continue;
-	}
-	//To substract
-	if(d=='-')
-	{
-		result=sub(a,b);
-		printf("\nDo you want to subtract more no?\nPress 1 for YES\nPress 2 for NO");
-		scanf("%d",&choice);
+		printf("press any one operation from the following: '+', '-', '*', '/', '^', '=': ");
+		scanf("%c",&d);
 		
-		if(choice==1)
-		{
-			printf("\nEnter new no: ");
-			scanf("%d",&a);
-		}
-		else if(choice==0)
-		{
-			printf("\nTotal= %d",result);
-		}
-		continue;
 	}
-	//To multiply
-	if(d=='*')
-	{
-		result=mult(a,b);
-		printf("\nDo you want to multiply more no?\nPress 1 for YES\nPress 2 for NO");
-		scanf("%d",&choice);
-		
-		if(choice==1)
-		{
-			printf("\nEnter new no: ");
-			scanf("%d",&a);
-		}
-		else if(choice==0)
-		{
-			printf("\nTotal= %d",result);
-		}
-		continue;
-	}
-	
-	
+	printf("\nThe result= %d",result);
 	return 0;
-	
 }
